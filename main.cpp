5e0c8387b1770057057467d267adcf99bdd1908b
lvthanh18apcs <47595741+lvthanh18apcs@users.noreply.github.com>
2019-04-03 21:47:39 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+
+int main()
+{
+	int sum = 0;
+	while (1 != 0)
+	{
+		int n;
+		cin >> n;
+		if (n == 0) break;
+		else
+		{
+			if (n < 2 || (n % 2 == 0 && n!=2)) sum -= n;
+			else
+			{
+				for (int i = 3; i <= sqrt(n); ++i)
+				{
+					if (n % i == 0)
+					{
+						sum -= n;
+						break;
+					}
+				}
+			}
+			sum += n;
+		}
+	}
+	cout << "Sum of prime number(s) is: " << sum;
+	system("pause");
+	return 0;
+}
+
