1030a1795ec9eb4ad870221ba12d390e4340a5f1
PantheraXpoint <48640044+PantheraXpoint@users.noreply.github.com>
2019-10-07 08:48:00 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,33 +3,5 @@
 
 int main()
 {
-	SLL quang;
-	int x, n, y; cin >> n >> y;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> x;
-		quang.InsertLast(x);
-	}
-	bool k = quang.DeleteAnode(y);
-	cout << endl;
-	if (k == true) cout << "true\n";
-	else cout << "false\n";
-	quang.Display();
-	system("pause");
 	return 0;
 }
-
-/*
-SLL quang;
-	int x, n, y; cin >> n >> y;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> x;
-		quang.InsertBegin(x);
-	}
-	bool k = quang.DeleteAnode(y);
-	cout << endl;
-	if (k == true) cout << "true\n";
-	else cout << "false\n";
-	quang.Display();
-*/
